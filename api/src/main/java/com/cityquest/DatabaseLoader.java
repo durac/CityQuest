29cb935e700cdde50c3d8f5b7164a0fe0caccaf2
Dominik Schwarz <dominik.schwarz1994@gmail.com>
2017-07-20 18:24:54 +0200

    add riddle list

--- api/src/main/java/com/cityquest/DatabaseLoader.java
+++ api/src/main/java/com/cityquest/DatabaseLoader.java
@@ -1,7 +1,7 @@
 package com.cityquest;
 
 import com.cityquest.persistence.dbo.Riddle;
-import com.cityquest.persistence.dbo.RiddleGenre;
+import com.cityquest.persistence.dbo.RiddleCategory;
 import com.cityquest.persistence.dbo.RiddleType;
 import com.cityquest.persistence.repository.RiddleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,18 +25,183 @@ public class DatabaseLoader implements CommandLineRunner {
 	public void run(String... strings) throws Exception {
 		this.repository.save(new Riddle("Riddle #1",
 				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
-				"Noon", RiddleType.NORMAL, RiddleGenre.LOGIC));
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
 		this.repository.save(new Riddle("Riddle #2",
 				"Laughing Out?",
-				"Loud", RiddleType.NORMAL, RiddleGenre.LOGIC));
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
 		this.repository.save(new Riddle("Riddle #3",
 				"What five-letter word becomes shorter when you add two letters to it?",
-				"Short", RiddleType.NORMAL, RiddleGenre.LOGIC));
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
 		this.repository.save(new Riddle("Riddle #4",
 				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
-				"Post Office", RiddleType.NORMAL, RiddleGenre.LOGIC));
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
 		this.repository.save(new Riddle("Riddle #5",
 				"Where do fish keep their money?",
-				"Riverbank", RiddleType.NORMAL, RiddleGenre.LOGIC));
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #6",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #7",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #8",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #9",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #10",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #11",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #12",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #13",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #14",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #15",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #21",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #22",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #23",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #24",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #25",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #26",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #27",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #28",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #29",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #30",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #31",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #32",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #33",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #34",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #35",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #41",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #42",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #43",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #44",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #45",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #46",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #47",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #48",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #49",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #50",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #51",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #52",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #53",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #54",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #55",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #61",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #62",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #63",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #64",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #65",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #66",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #67",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #68",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #69",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #70",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #71",
+				"What time of day, when written in a capital letters, is the same forwards, backwards and upside down?",
+				"Noon", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #72",
+				"Laughing Out?",
+				"Loud", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #73",
+				"What five-letter word becomes shorter when you add two letters to it?",
+				"Short", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #74",
+				"What starts with a 'P', ends with an 'E' and has thousands of letters?",
+				"Post Office", RiddleType.NORMAL, RiddleCategory.LOGIC));
+		this.repository.save(new Riddle("Riddle #75",
+				"Where do fish keep their money?",
+				"Riverbank", RiddleType.NORMAL, RiddleCategory.LOGIC));
 	}
 }
--- app/App.js
+++ app/App.js
@@ -1,23 +1,114 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import { List, ListItem, SearchBar } from "react-native-elements";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu!</Text>
-      </View>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        this.state = {
+            loading: false,
+            data: [],
+            page: 0,
+            refreshing: false
+        };
+    }
+
+    componentDidMount() {
+        this.makeRemoteRequest();
+    }
+
+    makeRemoteRequest = () => {
+        const { page } = this.state;
+        const url = `http://192.168.178.60:8080/api/riddles?page=${page}&size=20`;
+        this.setState({ loading: true });
+
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    data: page === 0 ? res._embedded.riddles : [...this.state.data, ...res._embedded.riddles],
+                    loading: false,
+                    refreshing: false
+                });
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    handleRefresh = () => {
+        this.setState(
+            {
+                page: 0,
+                refreshing: true
+            },
+            () => {
+                this.makeRemoteRequest();
+            }
+        );
+    };
+
+    handleLoadMore = () => {
+        this.setState(
+            {
+                page: this.state.page + 1
+            },
+            () => {
+                this.makeRemoteRequest();
+            }
+        );
+    };
+
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: "100%",
+                    backgroundColor: "#CED0CE",
+                }}
+            />
+        );
+    };
+
+    renderFooter = () => {
+        if (!this.state.loading) return null;
+
+        return (
+            <View
+                style={{
+                    paddingVertical: 20,
+                    borderTopWidth: 1,
+                    borderColor: "#CED0CE"
+                }}
+            >
+                <ActivityIndicator />
+            </View>
+        );
+    };
+
+    render() {
+        return (
+            <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={({ item }) => (
+                        <ListItem
+                          title={item.name}
+                          subtitle={`${item.type} ${item.category}`}
+                          containerStyle={{ borderBottomWidth: 0 }}
+                        />
+                     )}
+                    keyExtractor={item => item.name}
+                    ItemSeparatorComponent={this.renderSeparator}
+                    ListFooterComponent={this.renderFooter}
+                    onRefresh={this.handleRefresh}
+                    refreshing={this.state.refreshing}
+                    onEndReached={this.handleLoadMore}
+                    onEndReachedThreshold={50}
+                />
+            </List>
+        );
+    }
+}
